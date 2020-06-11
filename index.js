c86b791064eb37ed64cc2135d58339baff6fc776
Pablo Klaschka <contact@pabloklaschka.de>
2020-06-11 17:07:44 +0200

    Added linting config

--- index.js
+++ index.js
@@ -3,25 +3,29 @@
  * Copyright (c) 2020 by Pablo Klaschka
  */
 
-const {program} = require('commander');
-const checkDependencies = require('./helpers/dependency-check');
-const readConfig = require('./helpers/read-config');
-const processDita = require('./helpers/process-dita');
+const { program } = require("commander");
+const checkDependencies = require("./helpers/dependency-check");
+const readConfig = require("./helpers/read-config");
+const processDita = require("./helpers/process-dita");
 
 program
-    .name('dita-ot-helper')
-    .version(require('./package.json').version)
-    .description('A little helper for automating some of the more tedious tasks of automation with the DITA Open Toolkit');
+  .name("dita-ot-helper")
+  .version(require("./package.json").version)
+  .description(
+    "A little helper for automating some of the more tedious tasks of automation with the DITA Open Toolkit"
+  );
 
 let confFilePath = null;
 
-program
-    .arguments('<conf>')
-    .action((conf) => {
-        confFilePath = conf;
-    });
+program.arguments("<conf>").action((conf) => {
+  confFilePath = conf;
+});
 
-program.option('-v --verbose', 'Activate verbose output of DITA-OT commands', false);
+program.option(
+  "-v --verbose",
+  "Activate verbose output of DITA-OT commands",
+  false
+);
 
 program.parse(process.argv);
 
@@ -32,13 +36,13 @@ processConfigFile(confFilePath);
  * @param {string} configFilePath The path to the JSON config file
  */
 function processConfigFile(configFilePath) {
-    console.info('> Checking dependencies');
-    checkDependencies(['dita']);
-    console.info('> Dependency check complete');
+  console.info("> Checking dependencies");
+  checkDependencies(["dita"]);
+  console.info("> Dependency check complete");
 
-    console.info('> Reading config file');
-    const config = readConfig(configFilePath);
-    console.info('> Processing config file');
-    processDita(configFilePath, config, !program['verbose']);
-    console.info('> Compilation complete.')
+  console.info("> Reading config file");
+  const config = readConfig(configFilePath);
+  console.info("> Processing config file");
+  processDita(configFilePath, config, !program["verbose"]);
+  console.info("> Compilation complete.");
 }
